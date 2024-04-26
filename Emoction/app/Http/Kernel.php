1f0b9525b882d13011a358cc0e155c767615a40b
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-04-26 20:36:05 +0200

    ok configurazione iniziale fatta

--- Emoction/app/Http/Kernel.php
+++ Emoction/app/Http/Kernel.php
@@ -36,7 +36,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            // \App\Http\Middleware\HandleInertiaRequests::class, // Rimuovi questo middleware se non desideri Inertia
+            \App\Http\Middleware\HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ],
 
         'api' => [
--- Emoction/vite.config.js
+++ Emoction/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import 'dotenv/config';
 
 export default defineConfig({
-    build: {
-        minify: process.env.APP_ENV === 'production' ? 'esbuild' : false,
-        cssMinify: process.env.APP_ENV === 'production',
-    },
     plugins: [
         laravel({
-            input: ['resources/react/app.tsx'],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
         react(),
